1a9e427c0df30b212508af10cb96f9134a958be1
pablo-a <pabril@student.42.fr>
2017-03-16 18:31:17 +0100

    correct minor mistales + add gitignore

--- parse_transformed_pdf.py
+++ parse_transformed_pdf.py
@@ -2,6 +2,7 @@
 import re
 from pablo import Pablo
 import time
+import os
 
 def parse_fruits(data):#nul produit
     # group 1 is FRUITS
@@ -11,7 +12,6 @@ def parse_fruits(data):#nul produit
     accented_char = "àèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ"
     pattern = r"[\r]*([A-Z" + accented_char + r"]+)(.*)(\d,\d\d).*([-+]\d+(,\d+)? %|=)"
     results = re.findall(pattern, data)
-    print("nb result = %s" % len(results))
 
     for name, desc, price, evolution, rest in results:
         if name == "BOEUF" or name == "VEAU" or name == "PORC":
@@ -26,20 +26,23 @@ def parse_fruits(data):#nul produit
 
 
 
-def some(path, database=None):
+def some(path):
     bdd = Pablo()
-    date = re.sub(r".*(\d{8}).*", "\1", path)
-    req = """INSERT INTO fruit
+    date = re.sub(r".*(\d{8}).*", r"\1", path)
+    print(date)
+    req = """INSERT INTO fruit_vegetable
             (product, description, price, evolution, date_extract, date_price)
             VALUES (%s, %s, %s, %s, %s, %s)"""
     with open(path, "r") as file:
         #print(type(file.read().decode('utf-8')))
-        for item in parse(file.read()):
+        for item in parse_fruits(file.read()):
             #insert item in database.
             params = (item['name'], item['desc'], item['price'],
                         item['evolution'], time.strftime("%Y%m%d"), date)
-            bdd.exec_req_with_args(req, params)
-        bdd.commit()
-        bdd.close()
+            #bdd.exec_req_with_args(req, params)
 
-some("pdfminer.txt")
+    bdd.commit()
+    bdd.close()
+
+
+some("pdfminer20170315.txt")
